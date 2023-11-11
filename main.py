c41b11efe9f6274e4b804fe6820c66fc28e222d8
chukky01 <85424159+chukky01@users.noreply.github.com>
2023-11-11 11:21:38 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+class Solution:
+  def longestConsecutive(self, nums: List[int]) -> int:
+    ans = 0
+    seen = set(nums)
+
+    for num in nums:
+      if num - 1 in seen:
+        continue
+      length = 0
+      while num in seen:
+        num += 1
+        length += 1
+      ans = max(ans, length)
+
+    return ans
+
+#time complexity  = O(n)
+#space complexity = O(n)
+
